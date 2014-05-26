c7f0de71b71debb4d45cdfc98609bd57093359d5
Aaron Decker <deckeraa@uwplatt.edu>
2014-05-25 21:19:53 -0500

    Rearranged the order of the functions so that "lein ring server" loads the file. read-post-from-file now loads the blog post from a file.
    I realized that I wasn't sure what I even wanted from the Literate Clojure
    functionality. I suppose I should have written test cases first. #TDD and
    all that. Anyhow, I'm going to put the Literate Clojure part on hold and
    get the rest of the blog functionality working.
    While working with the defpost macro was fun, it lead to problems
    (read-string was taking out all comment lines ... like you would expect),
    and was really a distraction from the functionality that I actually need.

--- resources/public/foo.clj
+++ resources/public/foo.clj
@@ -1,8 +1,9 @@
-(defpost {:title "foo-title"}
+{:title "foo-title"}
 This is my test post.
-;> (+ 3 2)
+
 Are line breaks  preserved?
 
 How about <strong>bolded text</strong> due to html?
 
-Are 3 line breaks <br/><br/><br/> preserved?)
+Are 3 line breaks <br/><br/><br/> preserved?
+END OF foo.clj
--- src/cephalopod/handler.clj
+++ src/cephalopod/handler.clj
@@ -6,59 +6,54 @@
             [hiccup.core :as html]
             [me.raynes.conch :as conch]))
 
-(conch/programs pwd)
-
-(defroutes app-routes
-  (GET "/" [] (html/html [:html
-                          [:h1 "Hello World"]
-                          [:p "This is a test paragraph"]]))
-  (GET "/ioctopus/:foo" [foo] (htmlify-post foo))
-  (route/resources "/")
-  (route/not-found "Not Found"))
-
-(def app
-  (handler/site app-routes))
-
-(defn htmlify-post [post-title]
-  (html/html [:html
-              (pwd)
-              "<br/>"
-              (slurp (str "./resources/public/" post-title))
-              [:p post-title]])
-  )
 
 
-(read-string "(+ 2 3)")
+
+
+;; (read-string "(+ 2 3)")
 
 ;; returns a list with a map of post attributes as the first element
 ;; and a string with the post converted to html
 ;; (the newlines in the post are replaced by html breaks)
+;; (defmacro defpost [map & post]
+;;   `(let [processed-post# (str '~post)]
+;;      (list ~map processed-post#)))
 (defmacro defpost [map & post]
-  `(let [processed-post# (str '~post)]
-     (list ~map processed-post#)))
-(defmacro defpost [map & post]
-  `(let [processed-post# (str '~post)]
+  `(let [str-post# (str '~post)
+        stringified-post# (subs str-post# 1 (count str-post#))
+         processed-post# (exec-literate-prog stringified-post#)]
      (list
       ~map
-      processed-post#)))
-(defmacro defpost [map & post]
-  (list map (str 'post)))
-(eval (read-string (slurp "./resources/public/foo.clj")))
-(eval (macroexpand '(defpost {:title "bar"} asdf qwer))
-      )
-(defpost {:title "bar"} asdf qwer)
-(exec-literate-prog (slurp "./resources/public/foo.clj"))
-;; literate
-;; Takes a literal Clojure program as one big string and
-;; executes the lines marked for execution by starting with ;>
-;;
-(defn exec-literate-prog [prog]
-  (let [lines (clojure.string/split-lines prog)]
-    (clojure.string/join "\n" (remove nil? (map read-literate-string lines)))))
+      stringified-post#)))
+;; (load-file "./resources/public/foo.clj")
+;; (eval (read-string (slurp "./resources/public/foo.clj")))
+;; (load-string (slurp "./resources/public/foo.clj"))
+;; (defmacro defpost [map & post]
+;;   (list map (str 'post)))
+;; (eval (read-string (slurp "./resources/public/foo.clj")))
+;; (eval (macroexpand '(defpost {:title "bar"} asdf qwer))
+;;       )
 
-(deftest test-exec-literate-prog
-  (is (= (exec-literate-prog "foo\n;>(+ 4 3)\nbar") "foo\n7\nbar"))
-  (is (= (exec-literate-prog "foo\n;>(+ 4 3)\n;(+ 4 5)\n;another comment\nbar") "foo\n7\nbar")))
+;; (exec-literate-prog (slurp "./resources/public/foo.clj"))
+;; (macroexpand '(defpost {:title "bar"}  asdf foo \n qwer
+;;                ))
+;; (load-file "./resources/public/foo.clj")
+;; (read-string "#=(clojure.java.shell/sh \"echo\" \"hi\")")
+
+;; (exec-literate-prog (slurp "./resources/public/foo.clj"))
+
+(defn read-post-from-string [post]
+  (let [lines (clojure.string/split-lines post)
+        metadata (read-string (first lines))
+        post (rest lines)
+        htmlified-post (clojure.string/join "<br/>\n" post)]
+    (list metadata htmlified-post)))
+
+(deftest test-read-post-from-string
+  (is (= (read-post-from-string "{:title foo}\nHello World\nLine Two")
+         '({:title foo} "Hello World<br/>\nLine Two"))))
+
+
 
 ;; read-literate-string
 ;; Takes a string a reads it.
@@ -67,12 +62,12 @@
 ;; Otherwise the subsequent form after ";>" is
 ;; read and evaluted, and the return valued is
 ;; stringified and returned.
-(defn read-literate-string [string]
-  (if (= (subs string 0 2) ";>")
-    (pr-str (eval (read-string (subs string 2))))
-    (if (= (subs string 0 1) ";")
-      nil
-      string)))
+;; (defn read-literate-string [string]
+;;   (if (= (subs string 0 2) ";>")
+;;     (pr-str (eval (read-string (subs string 2))))
+;;     (if (= (subs string 0 1) ";")
+;;       nil
+;;       string)))
 
 (defn read-literate-string [string]
   (cond
@@ -91,3 +86,32 @@
   (is (= (read-literate-string "a") "a"))
   (is (= (read-literate-string ";a") nil))
   (is (= (read-literate-string "") nil)))
+
+;; literate
+;; Takes a literal Clojure program as one big string and
+;; executes the lines marked for execution by starting with ;>
+;;
+(defn exec-literate-prog [prog]
+  (let [lines (clojure.string/split-lines prog)]
+    (clojure.string/join "\n" (remove nil? (map read-literate-string lines)))))
+
+(deftest test-exec-literate-prog
+  (is (= (exec-literate-prog "foo\n;>(+ 4 3)\nbar") "foo\n7\nbar"))
+  (is (= (exec-literate-prog "foo\n;>(+ 4 3)\n;(+ 4 5)\n;another comment\nbar") "foo\n7\nbar")))
+
+(defn htmlify-post [post-title]
+  (html/html [:html
+              (second (read-post-from-string
+                       (slurp (str "./resources/public/" post-title))))
+]))
+
+(defroutes app-routes
+  (GET "/" [] (html/html [:html
+                          [:h1 "Hello World"]
+                          [:p "This is a test paragraph"]]))
+  (GET "/ioctopus/:foo" [foo] (htmlify-post foo))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
